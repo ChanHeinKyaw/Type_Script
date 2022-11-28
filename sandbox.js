52b408cc58f40ffa46b4ee9a0b69cbe27e5469f0
ChanHeinKyaw <scm.chanheinkyaw@gmail.com>
2022-11-28 10:43:10 +0630

    compiling-typescript

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+var charcter = "lario";
+console.log(charcter);
+var inputs = document.querySelectorAll("input");
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,8 @@
+const charcter = "lario"
+console.log(charcter);
+
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+    console.log(input);
+})
