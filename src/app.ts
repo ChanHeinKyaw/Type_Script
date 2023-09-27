9a4568d32fce1d918ff5c74c3519c4c969c1878e
MyatMon-MTM <scm.myatmon@gmail.com>
2023-09-27 16:39:04 +0630

    classes

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,35 @@
+//########################Chapter-11#######################//
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d: string, a: number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format(){
+        return `${this.client} owes ${this.amount} for ${this.details}`;
+    }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = []; // type ကို Invoice ကြေ ြငာထားလို့ Invoice class ကို invoke လုပ်ထားတဲ့ varible ပဲ ထည့်လို့ရမယ်
+//invoices.push('name'); // error 
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invOne.client = 'yoshi'; // overwrite mario
+invTwo.amount = 400; // overwrite 300
+
+console.log(invOne,invTwo);
+console.log(invoices);
+
+
 //########################Chapter-10#######################//
 // const anchor = document.querySelector('a')!;
 
